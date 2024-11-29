5ab3945c635fc55d02273c2ccd88fd8279267226
danielwzr <daniel.wzrjs@gmail.com>
2024-11-29 12:36:44 -0300

    add category filter

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// DARK MODE
 const toggleButton = document.getElementById('theme-toggle');
 const body = document.body;
 
@@ -8,3 +9,36 @@ toggleButton.addEventListener('click', () => {
     body.setAttribute('data-theme', 'dark');
   }
 });
+//
+
+
+
+
+
+//FILTRO
+const categoryFilter = document.getElementById('category-filter');
+const cards = document.querySelectorAll('.card');
+
+categoryFilter.addEventListener('change', (event) => {
+  const selectedCategory = event.target.value;
+  cards.forEach(card => {
+    const category = card.getAttribute('data-category');
+    if (selectedCategory === 'all' || category === selectedCategory) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+});
+//
+
+
+//MENU HAMBURGUER
+const hamburgerToggle = document.getElementById('hamburger-toggle');
+const menu = document.querySelector('nav ul');
+
+hamburgerToggle.addEventListener('click', () => {
+  hamburgerToggle.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+//
