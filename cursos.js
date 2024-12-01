dab51729e869bc2a7f6a6aa3d7b8262b58d12846
danielwzr <daniel.wzrjs@gmail.com>
2024-12-01 10:49:12 -0300

    dom

---
+++ cursos.js
@@ -0,0 +1,90 @@
+const cursos = [
+    {
+        id: 1,
+        nome: "Informática Básica",
+        descricao: "Aprenda o básico de informática para uso pessoal e profissional.",
+        cargaHoraria: 4,
+        categoria: "informatica",
+        urlImagem: "https://www.cursosrapidosgratis.com.br/img_cursos/prod/img_1230x644/informatica/informatica-basica.jpg",
+        urlApostila: "",
+        urlUdemy: "https://www.udemy.com/course/informatica-2024/?referralCode=93D830891F90E697924F",
+        disponivel: true
+    },
+    {
+        id: 2,
+        nome: "Excel Básico",
+        descricao: "Crie planilhas utilizando funções, formatando dados, gerando gráficos e automações para produtividade",
+        cargaHoraria: 5,
+        categoria: "informatica",
+        urlImagem: "https://genyo.com.br/wp-content/uploads/2024/04/controle-de-ponto-excel-768x474.jpg",
+        urlApostila: "",
+        urlUdemy: "https://www.udemy.com/course/curso-excel-completo-especialista/?referralCode=709706002653821AE66A",
+        disponivel: true
+    },
+    {
+        id: 3,
+        nome: "Lógica de Progamação",
+        descricao: "Entenda os fundamentos da lógica para aprender a programar.",
+        cargaHoraria: 0,
+        categoria: "programacao",
+        urlImagem: "https://idocode.com.br/wp-content/uploads/2021/07/code-1839406_1920-1.jpg",
+        urlApostila: "",
+        urlUdemy: "",
+        disponivel: false
+    },
+    {
+        id: 4,
+        nome: "Programação de Jogos 2D",
+        descricao: "Aprenda a criar jogos simples e divertidos em 2D.",
+        cargaHoraria: 0,
+        categoria: "programacao",
+        urlImagem: "https://ctcdigital.com.br/wp-content/uploads/2020/02/Curso-online-de-programa%C3%A7%C3%A3o-de-jogos-1.jpg",
+        urlApostila: "",
+        urlUdemy: "",
+        disponivel: false
+    },
+    {
+        id: 5,
+        nome: "Etapas no Desenvolvimento de Software",
+        descricao: "Conheça o ciclo completo do desenvolvimento de software.",
+        cargaHoraria: 0,
+        categoria: "",
+        urlImagem: "https://www.ziprecruiter.com/blog/wp-content/uploads/2022/03/Manager-Meets-with-Employees.jpg",
+        urlApostila: "",
+        urlUdemy: "",
+        disponivel: false
+    },
+    {
+        id: 6,
+        nome: "Programação Web Básica",
+        descricao: "Introdução ao desenvolvimento de sites e aplicações web.",
+        cargaHoraria: 0,
+        categoria: "",
+        urlImagem: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj8AWPbeExdXhYr9fcz2PPwLp2soY_jIXA2Uyzx2o04iMiHRxkNxCYEsd7QYl8c5Dd8gJBXpZOLpGwcCfCTL6n7p4SS5wy0SmI6Y6jxr-tIfcDU136U4i3ZuJj_CP-262sR2skE9UETElZS/s1600/26-9-2018-g-web.jpg",
+        urlApostila: "",
+        urlUdemy: "",
+        disponivel: false
+    },
+    {
+        id: 7,
+        nome: "Design System no Figma",
+        descricao: "Crie e gerencie sistemas de design utilizando o Figma.",
+        cargaHoraria: 0,
+        categoria: "",
+        urlImagem: "https://s3-alpha.figma.com/hub/file/5975417067/52dad0ba-0959-477d-888a-2c4b6c5d543d-cover.png",
+        urlApostila: "",
+        urlUdemy: "",
+        disponivel: false
+    },
+    {
+        id: 8,
+        nome: "Análise de Dados",
+        descricao: "Descubra como trabalhar com análise de dados e visualização.",
+        cargaHoraria: 0,
+        categoria: "",
+        urlImagem: "https://blog.zooxsmart.com/hubfs/Capa---Artigo-de-Blog_-Analise-de-dados.jpg",
+        urlApostila: "",
+        urlUdemy: "",
+        disponivel: false
+    },
+]
--- script.js
+++ script.js
@@ -11,34 +11,50 @@ toggleButton.addEventListener('click', () => {
 });
 //
 
-
-
-
-
-//FILTRO
-const categoryFilter = document.getElementById('category-filter');
-const cards = document.querySelectorAll('.card');
-
-categoryFilter.addEventListener('change', (event) => {
-  const selectedCategory = event.target.value;
-  cards.forEach(card => {
-    const category = card.getAttribute('data-category');
-    if (selectedCategory === 'all' || category === selectedCategory) {
-      card.style.display = 'block';
-    } else {
-      card.style.display = 'none';
-    }
-  });
-});
-//
-
-
 //MENU HAMBURGUER
-const hamburgerToggle = document.getElementById('hamburger-toggle');
+/*const hamburgerToggle = document.getElementById('hamburger-toggle');
 const menu = document.querySelector('nav ul');
 
 hamburgerToggle.addEventListener('click', () => {
   hamburgerToggle.classList.toggle('active');
   menu.classList.toggle('active');
-});
+});*/
+//
+
+
+// MONTA OS CARDS 
+const cardListHTML = document.getElementById("cards")
+
+cursos.forEach(curso =>{
+  const divHTML = document.createElement("div")
+  divHTML.className="card"
+  divHTML.setAttribute("data-category", curso.categoria)
+  divHTML.innerHTML = `
+          <div class="card-aviso">Curso em desenvolvimento</div>
+          <img src="${curso.urlImagem}" alt="${curso.nome}">
+          <h3>${curso.nome}</h3>
+          <p>${curso.descricao}</p>
+          <div class="buttons">
+            <a href="#">Baixar Apostila</a>
+            <a href="${curso.urlUdemy}">Acessar Curso</a>
+          </div>`
+  cardListHTML.appendChild(divHTML)
+})
+//
+
+//FILTRO
+  const categoryFilter = document.getElementById('category-filter');
+  const cards = document.querySelectorAll('.card');
+  
+  categoryFilter.addEventListener('change', (event) => {
+    const selectedCategory = event.target.value;
+    cards.forEach(card => {
+      const category = card.getAttribute('data-category');
+      if (selectedCategory === 'all' || category === selectedCategory) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  });
 //
