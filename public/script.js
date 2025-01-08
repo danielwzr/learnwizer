1bcc9a42d1dc8e2c7d14dd490ccba98a1a0b7d3c
danielwzr <daniel.wzrjs@gmail.com>
2025-01-08 19:57:10 -0300

    favicon

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 // DARK MODE
+/*
 const toggleButton = document.getElementById('theme-toggle');
 const body = document.body;
 
@@ -9,6 +10,7 @@ toggleButton.addEventListener('click', () => {
     body.setAttribute('data-theme', 'dark');
   }
 });
+*/
 //
 
 //MENU HAMBURGUER
@@ -24,22 +26,28 @@ hamburgerToggle.addEventListener('click', () => {
 
 // MONTA OS CARDS 
 const cardListHTML = document.getElementById("cards")
-
+function montarCards(){
+cardListHTML.innerHTML = "";
 cursos.forEach(curso => {
-  const divHTML = document.createElement("div")
-  divHTML.className = "card"
-  divHTML.setAttribute("data-category", curso.categoria)
-  divHTML.innerHTML = `
+  if (curso.disponivel) {
+    const divHTML = document.createElement("div")
+    divHTML.className = "card"
+    divHTML.setAttribute("data-category", curso.categoria)
+    divHTML.innerHTML = `
           <div class="card-aviso">Curso em desenvolvimento</div>
-          <img src="${curso.urlImagem}" alt="${curso.nome}">
-          <h3>${curso.nome}</h3>
-          <p>${curso.descricao}</p>
+         <a href="${curso.urlUdemy}"><img src="${curso.urlImagem}" alt="${curso.nome}"></a>
+          <a href="${curso.urlUdemy}"><h3>${curso.nome}</h3></a>
+          <a href="${curso.urlUdemy}"><p>${curso.descricao}</p></a>
           <div class="buttons">
             <a href="apostilas/${curso.arquivoApostila}">Apostila</a>
             <a href="${curso.urlUdemy}">Acessar Curso</a>
           </div>`
-  cardListHTML.appendChild(divHTML)
+    cardListHTML.appendChild(divHTML)
+  }
 })
+}
+
+montarCards();
 //
 
 //FILTRO
@@ -47,16 +55,23 @@ const categoryFilter = document.getElementById('category-filter');
 const cards = document.querySelectorAll('.card');
 
 categoryFilter.addEventListener('change', (event) => {
+  montarCards()
+  let qtd = 0;
   const selectedCategory = event.target.value;
   cards.forEach(card => {
     const category = card.getAttribute('data-category');
     if (selectedCategory === 'all' || category === selectedCategory) {
       card.style.display = 'block';
+      qtd++;
     } else {
       card.style.display = 'none';
     }
   });
+  if(qtd==0){
+    cardListHTML.innerHTML = "<div class='not-found'><img src='notfoundicon.png'><h2>Nenhum curso encontrado.</h2></div>"
+  }
 });
+
 //
 
 
@@ -70,7 +85,7 @@ function enviarSugestao() {
     modal.classList.add("show");
     inputSugestaoHTML.value = ""
   }
-  else{
+  else {
     inputSugestaoHTML.classList.add("input-error");
 
     // Remover a animação após a conclusão
@@ -87,8 +102,8 @@ function fecharModal() {
   modal.classList.add("hidden");
 }
 
-document.getElementById("modal-thanks").addEventListener("click", (ev)=>{
-  if(ev.target.id == "modal-thanks"){
+document.getElementById("modal-thanks").addEventListener("click", (ev) => {
+  if (ev.target.id == "modal-thanks") {
     fecharModal()
   }
 })
